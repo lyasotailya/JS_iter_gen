bc270c896811d92949c3200204813b63ff6ffda7
illyr <lysota2014@yandex.ru>
2024-12-04 20:39:58 +0300

    first

---
+++ gen/src/js/__tests__/PB.test.js
@@ -0,0 +1,18 @@
+import Person from '../Person';
+import Team from '../Team';
+
+const team = new Team();
+
+test('class "Team" has the "members" property with Set object', () => {
+  expect(team.members).toBeDefined();
+  expect(team.members instanceof Set).toBeTruthy();
+});
+
+team.members.add(new Person('person'));
+
+test('instance of class "Team" has a generator', () => {
+  const result = () => [...team];
+  expect(result).not.toThrow();
+  expect(team[Symbol.iterator].constructor.name).toBe('GeneratorFunction');
+  expect(result()).toEqual([...team.members]);
+});
---
+++ iter/src/js/app.js
@@ -0,0 +1,16 @@
+'use strict'
+
+import { Character } from './character'
+import { Bowman } from './character'
+import { Swordsman } from './character'
+import { Magician } from './character'
+import { Daemon } from './character'
+import { Undead } from './character'
+
+
+console.log(new Character('Monster', 'Daemon'))
+console.log(new Bowman('Nick'))
+console.log(new Swordsman('Nick'))
+console.log(new Magician('Nick'))
+console.log(new Daemon('Nick'))
+console.log(new Undead('Nick'))
